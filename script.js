7c3ec682e8fbfb11f06e04964434f9b143ca60ff
tymur.radun <30280128@bsdc.ac.uk>
2025-09-29 17:31:55 +0100

    test

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("terminal");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = 400;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+function randomChar() {
+    const chars = "01";
+    return chars[Math.floor(Math.random() * chars.length)];
+}
+
+function draw() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#0f0"; // зелёный текст
+    ctx.font = "16px monospace";
+
+    for (let i = 0; i < canvas.width / 10; i++) {
+        const x = i * 10;
+        const y = Math.random() * canvas.height;
+        ctx.fillText(randomChar(), x, y);
+    }
+}
+
+setInterval(draw, 50);
